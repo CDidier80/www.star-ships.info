3438030cf55e221f04c16d081b82dcb0aa38477d
Collin Didier <collin.didier80@gmail.com>
2020-12-15 22:40:01 -0600

    rename to include "page"

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@
 import React, { Component, useState, useEffect } from 'react'
 import {Route, Switch} from 'react-router-dom'
 import './styles/App.css';
-import Home from './pages/Home'
+import HomePage from './pages/HomePage'
 import StarshipPage from './pages/StarshipPage.js'
 
 
@@ -18,42 +18,12 @@ import StarshipPage from './pages/StarshipPage.js'
 function App(props) {
   return (
     <div className="app">
-      {/* <NavigationPanel className="navigationPanel" routePaths={this.state.routePaths}/> */}
-
-
-      
       <Switch>
-        <Route {...props} exact path={"/"} component={Home} />
-        <Route {...props} path={'/starships'} component={StarshipPage} />
+        <Route exact path={"/"}    component={(props) => <HomePage     {...props} />} />
+        <Route path={'/starships'} component={(props) => <StarshipPage {...props} />} />
       </Switch>
     </div>
   )
 }
 
 export default App
-
-
-// Option 2: functional-component version 
-// note: using the function version prevents paths being passed as props from state,
-// so remove pathRoutes from state and hard code them into each route and nav link
-
-
-
-// export default App
-
-
-
-
-//                          Purpose and Hierarchy Position of App.js
-
-// App.js is the 2nd highest javascript file in the tree hierarchy. Use it to set the routes and paths for 
-// your application and connect them to their respective page components. App.js should be imported to 
-// index.js for configuration and rendering.
-
-
-//                          Notes on <Switch> and <Route> components
-
-// These are components built in to React-Router to facilitate "multi-page" navigation. Switch components
-// prevent the performance-hindering effects of rendering all routes simultaneously. Route components are used
-// to connect the various pages of the application to a path. These paths are used to facilitate user navigation
-// between the pages. Each Route component requires a path and component prop to function.
