2dc57c9e3feadcf8a6e4e3281ccbf4a31d340b28
Collin Didier <collin.didier80@gmail.com>
2020-12-15 22:48:24 -0600

    del notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
-// See notes on App.js in React-Router context, <Switch> and <Route> below 
-
 import React, { Component, useState, useEffect } from 'react'
 import {Route, Switch} from 'react-router-dom'
 import './styles/App.css';
 import HomePage from './pages/HomePage'
 import StarshipPage from './pages/StarshipPage.js'
-
-
-// let appStyles = {
-//   video: {
-//     width: "500px",
-//     "zIndex": "100"
-//   }
-// }
-
 
 function App(props) {
   return (
