d9293691fd98384556ed562d7f7db525111e6c21
Collin Didier <collin.didier80@gmail.com>
2020-12-17 11:26:23 -0600

    all updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Route, Switch} from 'react-router-dom'
+import ApiClient from './Services/ApiClient'
 import './styles/App.css';
 import HomePage from './pages/HomePage'
 import StarshipPage from './pages/StarshipPage.js'
 
-function App(props) {
+function App() {
+
+  const [pageIsLoaded, changeLoadedBoolean] = useState(false)
+  const [starships, loadStarships] = useState([])
+
+  // useEffect(() => {
+  //   console.log("useEffect reached")
+  //   const getShips = async () => {
+  //     const responsePage1 = await ApiClient.get("/")
+  //     const responsePage2 = await ApiClient.get("/?page=2")
+  //     const responsePage3 = await ApiClient.get("/?page=3")
+  //     const responsePage4 = await ApiClient.get("/?page=4")
+  //     const combinedShipArray = [...responsePage1.data.results, ...responsePage2.data.results,
+  //       ...responsePage3.data.results, ...responsePage4.data.results
+  //     ]
+  //     console.log("combined ship array: ", combinedShipArray)
+  //     loadStarships(combinedShipArray)
+  //     if (!pageIsLoaded) {
+  //       changeLoadedBoolean(true)
+  //     }
+  //   }
+  //   getShips()
+  //   console.log(pageIsLoaded)
+  // }, []) 
+
   return (
     <div className="app">
       <Switch>
-        <Route exact path={"/"}    component={(props) => <HomePage     {...props} />} />
-        <Route path={'/starships'} component={(props) => <StarshipPage {...props} />} />
+        <Route exact path={"/"} component={
+          (props) => <HomePage {...props} starships={starships} pageIsLoaded={pageIsLoaded}/>} 
+        />
+        <Route path={'/starships'} component={
+          (props) => <StarshipPage {...props} />} 
+          />
       </Switch>
     </div>
   )
