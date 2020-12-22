b6fdac8d6124ae2b297f2cab997c80e3b40c0729
Collin Didier <collin.didier80@gmail.com>
2020-12-22 15:18:23 -0600

    update list with reliable urls

--- client/src/imageUrls.js
+++ client/src/imageUrls.js
@@ -1,40 +1,86 @@
 const imageUrls = [
     `url("https://i.pinimg.com/originals/43/54/d3/4354d36812bdd8049b6e4a0f1a34e3cc.jpg")`,
-    `url("https://cdn-3d.niceshops.com/upload/image/product/medium/default/revell-model-set-imperial-star-destroyer-1-pc-311141-en.jpg")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/5/5b/Imperial_Sentinel-class_shuttle.png")`,
+
+    // star destroyer -- my imgbb account
+    `url("https://i.ibb.co/Wc7KGMw/star-destroyer.jpg")`,
+
+    // sentinal class landing craft (empire) -- my imgbb account
+    `url("https://i.ibb.co/Lh1RwMh/Sentinel-class-Shuttle.jpg")`,
     `url("https://hughesenv.com/wp-content/uploads/2015/10/death-star.jpg")`,
     `url("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/veh-ia-1751-1576604159.jpg")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/c/cd/Ywing.jpg")`,
-    `url("https://upload.wikimedia.org/wikipedia/en/7/7b/X-wing.jpg")`,
-    `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyMMjGBTBZo_W2aW943LNUEix0qM93x_Rgzg&usqp=CAU")`,
-    `url("https://static.wikia.nocookie.net/awakening-of-the-rebellion/images/9/98/AotR_Executor.png")`,
+
+    // Y Wing - my imgbb account
+    `url("https://i.ibb.co/vxwVYt5/Y-Wing.jpg")`, 
+    // x wing - my imgbb account
+    `url("https://i.ibb.co/ZxdML2w/xwing.jpg")`,
+
+    // tie advanced x1 -- my imgbb account
+    `url("https://i.ibb.co/P9TMm7P/tie-advanced.jpg")`,
+
+    // executor -- my imgbb account
+    `url("https://i.ibb.co/26pwScF/Executor.png")`,
     `url("https://i.etsystatic.com/8461382/r/il/13be28/1200363838/il_570xN.1200363838_kp56.jpg")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/c/c7/Firespray_SotG.png")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/a/af/Shuttle-CHRON.jpg")`,
+
+    // Slave One (boba fett)  -- my imgbb account
+    `url("https://i.ibb.co/wWxG6SL/Screen-Shot-2020-12-21-at-10-25-30-AM.png")`,
+
+    // imperial shuttle  -- my imgbb account
+    `url("https://i.ibb.co/ZSKgL45/Imperial-Shuttle.png")`,
+
     `url("https://media.moddb.com/images/downloads/1/131/130947/Nebulon-B.jpg")`,
     `url("https://cdna.artstation.com/p/assets/images/images/005/389/734/large/howard-day-moncal-24-00000.jpg?1490690957")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/1/13/RZ1-BF2.png")`,
-    `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV8M388AuTF_mmo66Q1yWVGwgjoSdyjBdcpQ&usqp=CAU")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/0/00/ResoluteVSD.png")`,
+
+    // a-wing - my imgbb account
+    `url("https://i.ibb.co/ZVdWH6q/a-wing.png")`,
+
+    // b-wing - my imgbb account
+    `url("https://i.ibb.co/xmQWWYF/b-wing.jpg")`,
+
+    // republic cruiser -- my imgbb account
+    `url("https://i.ibb.co/5MYP11g/Republic-cruiser.png")`,
     `url("https://vignette.wikia.nocookie.net/battle-droids/images/5/5e/Droid_Control_Ship.png")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/0/01/Aaaaaaa.jpg")`,
+
+    // Naboo fighter - my imgbb account
+    `url("https://i.ibb.co/VxqB3Lx/naboo-fighter.png")`,
+
+
     `url("https://lumiere-a.akamaihd.net/v1/images/databank_nabooroyalstarship_01_169_e61f677e.jpeg?region=0%2C49%2C1560%2C780")`,
     `url("https://i.redd.it/y68np24w863z.jpg")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/2/2b/Royalcruiser.jpg")`,
+
+    // naboo type-j diplomatic barge - my imgbb account
+    `url("https://i.ibb.co/sqVRwFZ/j-type-diplomatic-barge.jpg")`,
     `url("https://lumiere-a.akamaihd.net/v1/images/aa-9-coruscant-freighter_a856053d.jpeg?region=92%2C0%2C1181%2C665&width=960")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/2/28/Delta-7_Interceptor_SoR.png")`,
-    `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQtup86A0Yoc18vwowZ9MEE7fba86Blb36lA&usqp=CAU")`,
+    `url("https://i.ibb.co/jGXb92V/jedi-starfighter.jpg")`,
+
+    // nubian yacht -- my imgbb account
+    `url("https://i.ibb.co/ZWVYwkK/nubian-yacht.jpg")`,
+
     `url("https://pm1.narvii.com/6870/cbb70179a4794ec72d378d55d0325e83a182eecer1-1920-1440v2_hq.jpg")`,
     `url("https://lumiere-a.akamaihd.net/v1/images/databank_geonosiansolarsailer_01_169_b3873578.jpeg?region=0%2C49%2C1560%2C780")`,
-    `url("https://static.wikia.nocookie.net/battlefront/images/2/26/Providence.jpg")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/8/8e/Theta-class_shuttle.png")`,
+
+    // trade federation cruiser -- my imgbb account
+    `url("https://i.ibb.co/dDLdYS1/trade-federation-cruiser.jpg")`,
+
+    // imperial theta class shuttle -- my imgbb account
+    `url("https://i.ibb.co/DrLWNpx/Imperial-Theta-Class-Shuttle.jpg")`,
     `url("https://i.ytimg.com/vi/VYCQS487LpA/maxresdefault.jpg")`,
-    `url("https://lh3.googleusercontent.com/proxy/kppUJ4-rkAT3-g0KStWfmciL22UiR1ZR0e2h9o7uoNtgNus9TAoZzbjYTc62Ki7KLUXYbaRe_PC7HrcnCyZ3gbbo6kHyoAtqBYPtbhmf69q_nD8wA-8rpIe5IXlOAYi88cG5Uzih3gEfEOd0qJe7PI05zsvpob4")`,
-    `url("https://static.wikia.nocookie.net/theclonewiki/images/6/68/AhsokaJediInterceptor-S.png")`,
+
+    // naboo star skiff --my imgbb account
+    `url("https://i.ibb.co/6bndLkj/Naboo-star-skiff-1.png")`,
+
+    // jedi interceptor --my imgbb account
+    `url("https://i.ibb.co/BfsW4K5/jedi-interceptor.jpg")`,
     `url("https://vignette.wikia.nocookie.net/greatmultiverse/images/b/ba/ARC170starfighter.jpg")`,
     `url("https://www.igorstshirts.com/blog/conceptships/2012/david_llewelyn/dl_01.jpg")`,
-    `url("https://pm1.narvii.com/6360/b3f106d951a3630965b37641a41498cd535bad6a_00.jpg")`,
-    `url("https://static.wikia.nocookie.net/starwars/images/6/66/Nimbus-class_V-wing_TFOWM.png")`
+
+    // belbullab 22  --my imgbb account
+    `url("https://i.ibb.co/2ZHMYf2/belbullab-22.jpg")`,
+
+    // V-Wing -- my imgbb account
+    `url("https://i.ibb.co/RhSmLB2/v-wing.png")`
 ]
 
 export default imageUrls 
+
+
+/* <blockquote class="imgur-embed-pub" lang="en" data-id="aywyRxp"><a href="https://imgur.com/aywyRxp">View post on imgur.com</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script> */
