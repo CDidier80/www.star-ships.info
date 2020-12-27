99c4339630ea27a57ffa4052f47ec2909b850fae
Collin Didier <collin.didier80@gmail.com>
2020-12-27 03:15:04 -0600

    updates

--- client/src/Services/ApiClient.js
+++ client/src/Services/ApiClient.js
@@ -1,6 +1,7 @@
 const { default: Axios } = require('axios')
 
-const url = process.env.NODE_ENV === 'production' ?  `${window.location.origin}` : 'http://localhost:3000' 
+// const url = process.env.NODE_ENV === 'production' ?  `${window.location.origin}` : 'http://localhost:3000' 
+const url = process.env.NODE_ENV === 'production' ?  `https://swapi.dev/api/starships` : 'http://localhost:3000' 
 const ApiClient = Axios.create({ baseURL: url })
 
 export default ApiClient
