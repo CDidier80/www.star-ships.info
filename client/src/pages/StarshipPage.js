8883f46c98134c85399df5a2927a67af9704da7a
Collin Didier <collin.didier80@gmail.com>
2020-12-27 15:32:13 -0600

    reformat

--- client/src/pages/StarshipPage.js
+++ client/src/pages/StarshipPage.js
@@ -3,12 +3,19 @@ import { backgroundImages, imageUrls } from '../imageUrls';
 import { NavLink } from 'react-router-dom';
 import "../styles/STARWARS/starwarsfont.css"
 
-
-function StarshipPage (props) {
+const StarshipPage = (props) => {
 
   const randomIndex = Math.floor(Math.random() * backgroundImages.length)
   const { state } = props.history.location
-  const {name, manufacturer, cost_in_credits, length, crew, passengers, starship_class } = state.starship
+  const {
+          name, 
+          manufacturer, 
+          cost_in_credits, 
+          length, 
+          crew, 
+          passengers, 
+          starship_class 
+        } = state.starship
   const { imageNum } = state
   const cost = cost_in_credits.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
   
@@ -26,65 +33,76 @@ function StarshipPage (props) {
   let starshipStyles = {
 
     appContentWrapper: {
-      margin: "0 auto",
-      width: "100%",
-      minHeight: "100vh",
-      overflowX: "hidden",
-      overflowY: "hidden",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      position: "relative"
+        margin: "0 auto",
+        width: "100%",
+        minHeight: "100vh",
+        overflowX: "hidden",
+        overflowY: "hidden",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        position: "relative"
     },
 
     pageBackgroundImg : {
-      position: "absolute",
-      minWidth: "100vw", 
-      minHeight: "100vh",
-      zIndex: "-100",
-      left: "0",
-      right: "0",
-      bottom: "0",
-      top: "0",
-      overflowX: "hidden", 
-      overflowY: "visible", 
-      backgroundImage: `url(${backgroundImages[randomIndex]})`,
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover"
+        position: "absolute",
+        minWidth: "100vw", 
+        minHeight: "100vh",
+        zIndex: "-100",
+        left: "0",
+        right: "0",
+        bottom: "0",
+        top: "0",
+        overflowX: "hidden", 
+        overflowY: "visible", 
+        backgroundImage: `url(${backgroundImages[randomIndex]})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
     },
 
     picture: {
-      margin: "0 auto",
-      minHeight: "400px",
-      minWidth: "265px",
-      maxHeight: "600px",
-      width: "78%",
-      maxWidth: "1100px",
-      marginTop: "2vh",
-      marginBottom: "2vh",
-      boxShadow: "0px 0px 15px white",
-      backgroundImage: `${imageUrls[imageNum]}`,
-      backgroundSize: "cover", 
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      border: "1px white solid",
-      borderRadius: "5px"
+        margin: "0 auto",
+        minHeight: "400px",
+        minWidth: "265px",
+        maxHeight: "600px",
+        width: "78%",
+        maxWidth: "1100px",
+        marginTop: "2vh",
+        marginBottom: "2vh",
+        boxShadow: "0px 0px 15px white",
+        WebkitBoxShadow: "0px 0px 15px white",
+        backgroundImage: `${imageUrls[imageNum]}`,
+        backgroundSize: "cover", 
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        border: "1px white solid",
+        borderRadius: "5px",
+        WebkitBorderRadius: "5px",
+        MozBorderRadius: "5px"
     }, 
 
     shipName: {
-      margin: "0 auto",
-      maxWidth: `${divMultiplier}px`,
-      textAlign: "center",
-      display: "block", 
-      marginTop: "40px",
-      marginBottom: "20px",
-      color: "black",
-      fontSize: "60px", 
-      fontFamily: 'Bebas Neue',
-      borderRadius: "4px", 
-      "padding": "10px", 
-      textShadow: "0 0 10px white, 0 0 20px #faffa3, 0 0 30px #f9ff8f, 0 0 40px #f9ff80, 0 0 50px #f8ff6b, 0 0 60px #f7ff61, 0 0 70px #f6ff4d"
+        margin: "0 auto",
+        maxWidth: `${divMultiplier}px`,
+        textAlign: "center",
+        display: "block", 
+        marginTop: "40px",
+        marginBottom: "20px",
+        color: "black",
+        fontSize: "60px", 
+        fontFamily: 'Bebas Neue',
+        borderRadius: "4px", 
+        WebkitBorderRadius: "4px",
+        MozBorderRadius: "4px",
+        padding: "10px", 
+        textShadow: `0 0 10px white, 
+                    0 0 20px #faffa3, 
+                    0 0 30px #f9ff8f, 
+                    0 0 40px #f9ff80, 
+                    0 0 50px #f8ff6b, 
+                    0 0 60px #f7ff61, 
+                    0 0 70px #f6ff4d`
     },
 
 
@@ -92,15 +110,27 @@ function StarshipPage (props) {
 
   return (
     <div style={starshipStyles.pageBackgroundImg}>
-      <main className="appContentWrapper" style={starshipStyles.appContentWrapper}>
-        <NavLink className="returnLink" style={starshipStyles.returnLink} to="/">Return</NavLink>
+      <main 
+          style={starshipStyles.appContentWrapper}
+          className="appContentWrapper" 
+      >
+        <NavLink 
+            className="returnLink" 
+            style={starshipStyles.returnLink} 
+            to="/">
+              Return
+        </NavLink>
         <div className="shipName" style={starshipStyles.shipName}>{name}</div>
         <div style={starshipStyles.picture}></div>
         <div className="detailWrapper">
           {infoArray.map((element, index) => (        
-              <div key={index} className="textWrapper" style={starshipStyles.textWrapper}>
-                <h3 className='shipDetail'>{element[0]}</h3>
-                <p className="dataPoint">{element[1]}</p>
+              <div 
+                  key={index} 
+                  className="textWrapper" 
+                  style={starshipStyles.textWrapper}
+              >
+                  <h3 className='shipDetail'>{element[0]}</h3>
+                  <p className="dataPoint">{element[1]}</p>
               </div>
               )) 
           }
