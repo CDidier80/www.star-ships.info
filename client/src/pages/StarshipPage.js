8a9a0c2dfc00cd5e48abdc4ff6327695b3d48959
Collin Didier <collin.didier80@gmail.com>
2021-03-14 22:55:41 -0500

    move styles into jss hooks move content into subcomponents

--- client/src/pages/StarshipPage.js
+++ client/src/pages/StarshipPage.js
@@ -1,143 +1,35 @@
-import React from 'react'
-import { backgroundImages, imageUrls } from '../imageUrls';
-import { NavLink } from 'react-router-dom';
+import { useStarshipPageStyles } from "./components/StarshipPage/usePageStyles"
+import ShipDetails from "./components/StarshipPage/ShipDetails"
+import Ship from "./components/StarshipPage/Ship"
 import "../styles/STARWARS/starwarsfont.css"
+import { NavLink } from 'react-router-dom'
+import React from 'react'
 
 const StarshipPage = (props) => {
 
-  const randomIndex = Math.floor(Math.random() * backgroundImages.length)
-  const { state } = props.history.location
-  const {
-          name, 
-          manufacturer, 
-          cost_in_credits, 
-          length, 
-          crew, 
-          passengers, 
-          starship_class 
-        } = state.starship
-  const { imageNum } = state
-  const cost = cost_in_credits.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-  
-  const infoArray = [
-    ["Starship Class", starship_class], 
-    ["Crew Size", crew], 
-    ["Passenger Capacity", passengers], 
-    ["Production Cost (credits)", cost], 
-    ["Manufacturer", manufacturer], 
-    ["Longest Dimension (m)", length]
-  ]
-
-  const divMultiplier = name.length * 24
-
-  let starshipStyles = {
-
-    appContentWrapper: {
-        margin: "0 auto",
-        width: "100%",
-        minHeight: "100vh",
-        overflowX: "hidden",
-        overflowY: "hidden",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        position: "relative"
-    },
-
-    pageBackgroundImg : {
-        position: "absolute",
-        minWidth: "100vw", 
-        minHeight: "100vh",
-        zIndex: "-100",
-        left: "0",
-        right: "0",
-        bottom: "0",
-        top: "0",
-        overflowX: "hidden", 
-        overflowY: "visible", 
-        backgroundImage: `url(${backgroundImages[randomIndex]})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover"
-    },
-
-    picture: {
-        margin: "0 auto",
-        minHeight: "400px",
-        minWidth: "265px",
-        maxHeight: "600px",
-        width: "78%",
-        maxWidth: "1100px",
-        marginTop: "2vh",
-        marginBottom: "2vh",
-        boxShadow: "0px 0px 15px white",
-        WebkitBoxShadow: "0px 0px 15px white",
-        backgroundImage: `${imageUrls[imageNum]}`,
-        backgroundSize: "cover", 
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        border: "1px white solid",
-        borderRadius: "5px",
-        WebkitBorderRadius: "5px",
-        MozBorderRadius: "5px"
-    }, 
-
-    shipName: {
-        margin: "0 auto",
-        maxWidth: `${divMultiplier}px`,
-        textAlign: "center",
-        display: "block", 
-        marginTop: "40px",
-        marginBottom: "20px",
-        color: "black",
-        fontSize: "60px", 
-        fontFamily: 'Bebas Neue',
-        borderRadius: "4px", 
-        WebkitBorderRadius: "4px",
-        MozBorderRadius: "4px",
-        padding: "10px", 
-        textShadow: `0 0 10px white, 
-                    0 0 20px #faffa3, 
-                    0 0 30px #f9ff8f, 
-                    0 0 40px #f9ff80, 
-                    0 0 50px #f8ff6b, 
-                    0 0 60px #f7ff61, 
-                    0 0 70px #f6ff4d`
-    },
+    const { state } = props.history.location
+    const { name } = state.starship
+    const { imageNum } = state
 
 
-  }
+    const classes = useStarshipPageStyles()
 
-  return (
-    <div style={starshipStyles.pageBackgroundImg}>
-      <main 
-          style={starshipStyles.appContentWrapper}
-          className="appContentWrapper" 
-      >
-        <NavLink 
-            className="returnLink" 
-            style={starshipStyles.returnLink} 
-            to="/">
-              Return
-        </NavLink>
-        <div className="shipName" style={starshipStyles.shipName}>{name}</div>
-        <div style={starshipStyles.picture}></div>
-        <div className="detailWrapper">
-          {infoArray.map((element, index) => (        
-              <div 
-                  key={index} 
-                  className="textWrapper" 
-                  style={starshipStyles.textWrapper}
-              >
-                  <h3 className='shipDetail'>{element[0]}</h3>
-                  <p className="dataPoint">{element[1]}</p>
-              </div>
-              )) 
-          }
+    const propsNavlink = {
+        className: classes.returnLink,
+        to: "/"
+    }
+
+    const propsShip = { name, imageNum }
+
+    return (
+        <div className={classes.pageBackgroundImg}>
+        <main className={classes.appContentWrapper} >
+            <NavLink {...propsNavlink} > Return </NavLink>
+            <Ship {...propsShip} />
+            <ShipDetails {...props} />
+        </main>
         </div>
-      </main>
-    </div>
-  )
+    )
 }
 
 export default StarshipPage;
