389b62784650fb9c3de059c60b6cf52327c80932
Collin Didier <collin.didier80@gmail.com>
2021-03-14 21:02:50 -0500

    reformat

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './App.js'
+import React from 'react'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
